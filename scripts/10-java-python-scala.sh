f04f018a99ef54f41b603c02bda7eb703f05f33e
Marcos Colebrook <marcos.colebrook@gmail.com>
2016-01-25 16:46:39 +0000

    Updated python installation script to add scikit-learn

--- scripts/10-java-python-scala.sh
+++ scripts/10-java-python-scala.sh
@@ -31,6 +31,10 @@ if [ "$SYSTEM" == "x86_64" ]; then
   sudo pip3 install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.6.0-cp34-none-linux_x86_64.whl  
 fi
 
+echo "#install sckit-learn"
+sudo pip install scikit-learn
+sudo pip3 install scikit-learn
+
 
 TEMP_DIR=/tmp
 
