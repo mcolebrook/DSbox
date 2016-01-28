e747f3a11bb2a83d64cd48b3daa31a0d775a24a9
Marcos Colebrook <marcos.colebrook@gmail.com>
2016-01-28 11:12:21 +0000

    Fixed installing scikit-learn message

--- scripts/10-java-python-scala.sh
+++ scripts/10-java-python-scala.sh
@@ -32,7 +32,7 @@ if [ "$SYSTEM" == "x86_64" ]; then
   sudo pip3 install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.6.0-cp34-none-linux_x86_64.whl  
 fi
 
-echo "#install sckit-learn"
+echo "# installing scikit-learn"
 sudo pip install scikit-learn
 sudo pip3 install scikit-learn
 
